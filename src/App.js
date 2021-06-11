738056ea686236541dfafa421b312e233a7b980a
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-06-12 07:34:30 +0900

    Create Organism

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButon } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButon } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "ゆーすけ",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "12345@example.com",
+  phone: "090-111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButon>検索</SecondaryButon>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
