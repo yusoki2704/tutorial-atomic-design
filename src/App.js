950d279f6fdecfa6c5b0f9bd9d8ecf15a9aaec37
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-06-13 07:31:35 +0900

    create pages

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButon } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButon } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "ゆーすけ",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "12345@example.com",
-  phone: "090-111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButon>テスト</PrimaryButon>
-        <SecondaryButon>検索</SecondaryButon>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
